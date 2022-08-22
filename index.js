cebcb0c9db138eb27dab7b56025ca7bf7290669c
devsammyy <yinkaadebayo31@gmail.com>
2022-08-22 22:28:19 +0100

    implement ejs template engine

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const router = require('./router')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const router = require("./router");
 
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(router)
+const myLogger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
+app.use(myLogger);
+app.use(router);
+
+app.use((req, res) => {
+  console.log("error");
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- router.js
+++ router.js
@@ -1,14 +1,18 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const data = require("./data/users");
 
-router.get('/', (req, res)=>{
-    res.render('./index')
-})
+router.get("/", (req, res) => {
+  res.render("./index");
+});
 
-router.post('/submit', (req, res) => {
-    const {text} = req.body
-    if(text === 'Babatunde')
-        res.render('./nextPage')
-}) 
+router.post("/submit", (req, res) => {
+  const { text } = req.body;
+  const answer = "4";
+  if (text === answer) {
+    res.render("./nextPage");
+  }
+  res.render("./error");
+});
 
 module.exports = router;
